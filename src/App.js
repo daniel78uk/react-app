84c433869f13b039593515ba8fb850e752fffc10
Marco Sangalli <sangalli.marco@gmail.com>
2017-04-10 22:08:04 +0200

    adding view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AddTodo from './containers/AddTodo'
+import ListItems from './containers/ListItems'
 
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <AddTodo />
+        <ListItems />
       </div>
     );
   }
--- src/containers/ListItems.js
+++ src/containers/ListItems.js
@@ -1,30 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { addTodo } from '../actions'
 
-let AddTodo = ({ dispatch }) => {
+let ListItems = ({ todos }) => {
+  let items = todos
 
   return (
     <ul>
-      <form onSubmit={e => {
-        e.preventDefault()
-        if (!input.value.trim()) {
-          return
-        }
-        dispatch(addTodo(input.value))
-        input.value = ''
-      }}>
-        <input ref={node => {
-          input = node
-        }} />
-        <button type="submit">
-          Add Todo
-        </button>
-      </form>
+      {
+        items.map(x=> (
+          <li key={x.id}>{x.text}</li>
+          )
+        ) 
+      }
     </ul>
   )
 }
+ListItems = connect(state => state)(ListItems)
 
-AddTodo = connect()(AddTodo)
-
-export default AddTodo
+export default ListItems
